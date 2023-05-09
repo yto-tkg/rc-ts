1bbece7b767b3513dcd2b215e0be1c329468e1fe
yto-tkg <yuto-takagi@cybozu.co.jp>
2023-05-10 08:56:51 +0900

    feat: setting state

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,26 @@
+import React, { useRef } from "react";
+
+type NewTodoProps = {
+  onAddTodo: (todoText: string) => void;
+};
+
+const NewTodo: React.FC<NewTodoProps> = (props) => {
+  const textInputRef = useRef<HTMLInputElement>(null);
+  const todoSubmitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    const enteredText = textInputRef.current!.value;
+    props.onAddTodo(enteredText);
+  };
+
+  return (
+    <form onSubmit={todoSubmitHandler}>
+      <div>
+        <label htmlFor="todo-text">TODO内容</label>
+        <input type="text" id="todo-text" ref={textInputRef} />
+      </div>
+      <button type="submit">TODO追加</button>
+    </form>
+  );
+};
+
+export default NewTodo;
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface TodoListProps {
+  items: { id: string; text: string }[];
+  onDeleteTodo: (id: string) => void;
+}
+
+const TodoList: React.FC<TodoListProps> = (props) => {
+  const { items, onDeleteTodo } = props;
+  return (
+    <ul>
+      {items.map((todo) => (
+        <li key={todo.id}>
+          <span>{todo.text}</span>
+          <button onClick={onDeleteTodo.bind(null, todo.id)}>削除</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
